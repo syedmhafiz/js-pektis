5039ce341d4d4e05427c38bfd686f557e49d5b89
syedmhafiz <syedhafiz61@gmail.com>
2026-01-02 05:45:30 +0600

    P2

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+function careForMe() {
+    let hitMe = 42;
+    let punchMe;
+    if (hitMe > 30) {
+        punchMe = 100;
+        console.log("I need a hug");
+    }
+    console.log(punchMe);
+}
+
+careForMe();
+
+function undef() {
+    let some;
+    console.log(some);
+}
+
+undef();
